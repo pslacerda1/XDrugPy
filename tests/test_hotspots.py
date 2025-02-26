a10232e1c7b18d94cf0b9023bad83e5a4f2eb114
Pedro Lacerda <pslacerda@gmail.com>
2025-02-26 12:46:00 -0300

    Better HO testing

--- tests/test_hotspots.py
+++ tests/test_hotspots.py
@@ -3,12 +3,12 @@ from xdrugpy.hotspots import load_ftmap, ho
 
 
 def test_ho():
-    ftmap = load_ftmap('data/A7YT55_6css_atlas.pdb', 'group')
+    load_ftmap('data/A7YT55_6css_atlas.pdb', 'group')
     assert ho('*D_01 *D_00', verbose=False) == 1
     assert ho('*D_01 *D_00 *B_05', verbose=False) == 0
     assert ho('*D_01 *D_00', output_sele='__a', verbose=False)
     assert pm.count_atoms('__a') == 184
     assert ho('*D_00 *D_01', output_sele='__a', verbose=False)
-    assert ho('*D_00 *D_01', output_sele='__a', verbose=False)
     assert pm.count_atoms('__a') == 184
-    assert pm.count_atoms('*D_00') > 184
+    assert ho('*D_00 *B_04', output_sele='__a', verbose=False)
+    assert pm.count_atoms('*D_00') > pm.count_atoms('*B_04') > pm.count_atoms('__a')
