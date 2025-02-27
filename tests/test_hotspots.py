64264398b258dd70bc7ae4fc27c9960903a6b0f1
Pedro Lacerda <pslacerda@gmail.com>
2025-02-27 08:45:12 -0300

    One more test case

--- tests/test_hotspots.py
+++ tests/test_hotspots.py
@@ -5,6 +5,7 @@ from xdrugpy.hotspots import load_ftmap, ho
 def test_ho():
     load_ftmap('data/A7YT55_6css_atlas.pdb', 'group')
     assert ho('*D_01 *D_00', verbose=False) == 371
+    assert ho('*D_00 *D_01', verbose=False) == 371
     assert ho('*D_01 *D_00 *B_05', verbose=False) == 0
     count = ho('*D_01 *D_00', output_sele='__a', verbose=False)
     assert pm.count_atoms('__a') == count
