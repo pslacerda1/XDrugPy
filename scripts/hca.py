f4ecdbd32670d56866574133953f051b3c128fdd
Pedro Sousa Lacerda <pslacerda@gmail.com>
2025-11-29 10:44:06 -0300

    Full power to power users

--- scripts/hca.py
+++ scripts/hca.py
@@ -1,18 +1,16 @@
-##############################################
-# HCA of a large number of hotspots
+##########################################
+# HCA of maybe a large number of hotspots
 #
-# Runs like:
+# Run it like:
 #    $ pymol -c hca.py
-#
-#############################################
+##########################################
 
 from os.path import expanduser, expandvars
 from glob import glob
-from xdrugpy.hotspots import load_ftmap, plot_pairwise_hca, SimilarityFunc
+from xdrugpy.hotspots import load_ftmap, plot_pairwise_hca, plot_euclidean_hca, SimilarityFunc, LinkageMethod
 
 from pymol import cmd as pm
 from matplotlib import pyplot as plt
-
 
 # OPTIONAL: Optimization that disables an unuseful feature if we'll not
 # launch the graphical interface after script execution.
@@ -31,19 +29,35 @@ for file in files[:25]:
     # plotting criteria. It also deletes the *.protein structures. This
     # optimization reduces the number of PyMOL objects irrelevant to our
     # analysis improving the performance at high loads.
-    for obj in pm.get_object_list("NOT (*.K15_* AND p.S0>25)"):
+    #                             "NOT (*.K15_* AND p.S0>20 OR *.protein)"
+    for obj in pm.get_object_list("NOT (*.K15_* AND p.S0>20)"):
         pm.delete(obj)
 
+# If you don't delete protein structures you can have a nice session!
+pm.save("~/My Folder/nice_session.pze")
+
 # Does the actual hierarchical cluster analysis (HCA) with hotspot overlap (HO)
-# function. Only very strong Kozakov2015 hotspots with at least p.S0>25 that
-# remained from the previous successive loading and deletions.
+# function. Only strong Kozakov2015 hotspots with at least p.S0>20 that remained
+# from the previous successive loading and deletions.
 plot_pairwise_hca(
-    '*.K15_* AND p.S0>25',      # all groups, K15 & S0>25, of any class
-    function=SimilarityFunc.HO, # superposition based hotspot similarity method
+    '*.K15_* AND p.S0>20',
+    function=SimilarityFunc.HO,
     align=False,                # suposes previously aligned structures (FTMove?)
-    radius=4,
-    color_threshold=0.7,        # probably you'll need to adjust this variable
-    hide_threshold=True,
-    annotate=False
+    radius=1.5,
+    color_threshold=0,          # probably you'll need to adjust this variable
+    hide_threshold=False,
+    annotate=True
+)
+plt.show()  # displays the HCA
+
+# A more standard type of HCA over the same hotspots of the previous analysis.
+# However this ones uses as distance the aggregation of all hotspot properties,
+# being the spatiality only part of the properties.
+plot_euclidean_hca(
+    '*.K15_* AND p.S0>20',
+    linkage_method=LinkageMethod.WARD,
+    color_threshold=0,
+    hide_threshold=False,
+    annotate=True,
 )
 plt.show()  # displays the HCA
