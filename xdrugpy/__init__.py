986bbd03f27253ffedbaece19ecea1d5c2990dc8
Pedro Sousa Lacerda <pslacerda@gmail.com>
2026-09-23 10:07:43 -0300

    Suporte ao Windows funciona?

--- xdrugpy/__init__.py
+++ xdrugpy/__init__.py
@@ -115,7 +115,7 @@ def xdrugpy_install(
             local_exe = RESOURCES_DIR / 'clustalo.exe'
             if not local_exe.exists():
                 urlretrieve(
-                    f"https://github.com/pslacerda1/XDrugPy/raw/refs/heads/master/bin/{web_name}",
+                    f"bhttps://github.com/pslacerda1/XDrugPy/raw/refs/heads/master/misc/{web_name}",
                     local_zip
                 )
                 import zipfile
